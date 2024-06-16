263bbb6742273c567a7ef2c1aa1ade9604c041eb
CarlosLeonc10-dev <carlosleok17@gmail.com>
2024-06-16 11:58:56 -0300

    Add files via upload

---
+++ Desafio-js/main.js
@@ -0,0 +1,76 @@
+ 
+// Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+alert("Hello Word")
+
+
+// Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+let numberOne = Number(prompt("Digite o primeiro número"))
+let numberTwo = Number(prompt("Digite o segundo número"))
+
+alert(`A soma dois dois números é ${numberOne + numberTwo}`)
+
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+numberOne = 2
+
+if( typeof numberOne == typeof "" ){
+   alert("É um número") 
+}else{
+    alert("Não é um número")
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let a = "hello"
+
+
+if(typeof a == typeof 1){
+    alert("É uma string")
+}else{
+    alert("Não é uma string")
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+
+let bolean = true
+
+if(typeof bolean == typeof "true"){
+    alert("É um booleano")
+}else{
+    alert("não é um booleano")
+}
+
+
+
+
+// Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+numberOne = 4
+numberTwo = 3
+
+alert(`A subtração é ${numberOne - numberTwo}`)
+
+// Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+alert(`A multiplicação é ${numberOne * numberTwo}`)
+
+// Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+alert(`A divisão é ${numberOne / numberTwo}`)
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par"
+
+if((numberOne % 2) == 0){
+    alert("É um número par")
+}else{
+    alert("Não é um número par")
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+if ((numberTwo % 2) != 0) {
+    alert("É um número ímpar")
+} else {
+    alert("Náo é um número ímpar")
+}
+
